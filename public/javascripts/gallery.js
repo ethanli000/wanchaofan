bf3871796fbd7cbea27eda817b273e2b5010e851
ethan-ubuntu <taozi8955@gmail.com>
2015-05-10 22:14:16 +0900

    add loading animation

--- public/javascripts/gallery.js
+++ public/javascripts/gallery.js
@@ -29,19 +29,31 @@ var setSize = function () {
     //80% height
     //$(".photo").height(gallery_height * 0.8);
     $(".photo").width(gallery_height * img_space_ratio * photo_ratio);
+    
     $(".photo img").height(gallery_height * img_space_ratio);
     $(".photo img").width(gallery_height * img_space_ratio * photo_ratio);
     $(".photo img").css("margin-top", gallery_height * (0.5 - img_space_ratio / 2));
-    $(".photo-count").width(gallery_height * img_space_ratio * photo_ratio);
+
+    $(".spin-container").css("top", gallery_height * (0.5 - img_space_ratio / 2));
+    $(".spin-container").width($(".photo img").width());
+    $(".spin-container").height($(".photo img").height());
+    
+    $(".photo-count").width($(".photo img").width());
     $(".photo-count").css("bottom", gallery_height * (0.5 - img_space_ratio / 2) - 30);
   } else {
     //80% width
     //$(".photo").height(gallery_width * 0.8 / photo_ratio);
     $(".photo").width(gallery_width * img_space_ratio);
+    
     $(".photo img").height(gallery_width * img_space_ratio / photo_ratio);
     $(".photo img").width(gallery_width * img_space_ratio);
     $(".photo img").css("margin-top", (gallery_height - gallery_width * img_space_ratio / photo_ratio) / 2);
-    $(".photo-count").width(gallery_width * img_space_ratio);
+    
+    $(".spin-container").css("top", (gallery_height - gallery_width * img_space_ratio / photo_ratio) / 2);
+    $(".spin-container").width($(".photo img").width());
+    $(".spin-container").height($(".photo img").height());
+
+    $(".photo-count").width($(".photo img").width());
     $(".photo-count").css("bottom", (gallery_height - gallery_width * img_space_ratio / photo_ratio) / 2 - 30);
   }
   return true;
@@ -57,26 +69,59 @@ var update_img = function () {
     data: {sort: current_sort},
     beforeSend: function () {
       is_img_loading = 1;
+      spin_start();
     },
     success: function (res) {
       if (res.result === "success") {
         $("<img/>").attr("src", res.data.url).load(function () {
           photo_ratio = this.width / this.height;
           setSize();
+          spin_stop();
           $(".photo img").attr("src", res.data.url);
+          $("span.photo-sort").text(current_sort);
         });
-        $("span.photo-sort").text(current_sort);
+        
       } else {
+        spin_stop();
         //$(".error-message").text("ADD FAILED: " + res.message);
       }
     },
     error: function () {
+      spin_stop();
       //$(".error-message").text("ADD FAILED: server error!");
     },
     complete: function () {
       is_img_loading = 0;
     },
   });
+};
+
+var spin_start = function () {
+  var opt = {
+    lines: 9, // The number of lines to draw
+    length: 8, // The length of each line
+    width: 4, // The line thickness
+    radius: 5, // The radius of the inner circle
+    corners: 1, // Corner roundness (0..1)
+    rotate: 0, // The rotation offset
+    direction: 1, // 1: clockwise, -1: counterclockwise
+    color: '#000', // #rgb or #rrggbb or array of colors
+    speed: 1.2, // Rounds per second
+    trail: 75, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 100, // The z-index (defaults to 2000000000)
+    top: '50%', // Top position relative to parent
+    left: '50%' // Left position relative to parent
+  };
+  $('#spin').spin(opt);
+  $(".spin-container").css("z-index", "100");
+};
+
+var spin_stop = function () {
+  $('#spin').spin(false);
+  $(".spin-container").css("z-index", "-1");
 };
 
 $(window).load(function () {
